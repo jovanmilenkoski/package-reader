5d70d6520511f3b03c419cf0514423b3aab4616e
s7c8pu_brancev <brance.veljanoski@alite-international.com>
2019-01-18 14:39:22 +0100

    Changed getName() method to remove scope from name, added new getFullName() method

--- src/reader.ts
+++ src/reader.ts
@@ -32,7 +32,25 @@ export class Reader {
         return this.package.version;
     }
 
+    /**
+     * Removes the scope of the package name. Returns only the name of the package.
+     * @returns {string} package name;
+     * @memberof Reader
+     */
     getName(): string {
+        const packageName = this.package.name;
+        const nameIsScoped = packageName.indexOf("@") !== -1 && packageName.lastIndexOf("/") !== -1;
+        return nameIsScoped ?
+            packageName.slice(packageName.lastIndexOf("/")) :
+            packageName;
+    }
+
+    /**
+     * Returns full package name.
+     * @returns {string} package name;
+     * @memberof Reader
+     */
+    getFullName(): string {
         return this.package.name;
     }
 
