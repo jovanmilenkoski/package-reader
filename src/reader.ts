f039ad75b282e6258fc0d084fbf42e93782fefe1
Jovan Milenkoski <jovanmilenkoski@Jovans-MacBook-Pro.local>
2019-01-22 11:31:03 +0100

    corrected getName() method

--- src/reader.ts
+++ src/reader.ts
@@ -35,17 +35,17 @@ export class Reader {
     getVersion(): string {
         return this.package.version;
     }
-
+    
     /**
-     * Removes the scope of the package name. Returns only the name of the package.
-     * @returns {string} package name;
-     * @memberof Reader
-     */
+         * Removes the scope of the package name. Returns only the name of the package.
+         * @returns {string} package name;
+         * @memberof Reader
+         */
     getName(): string {
         const packageName = this.package.name;
         const nameIsScoped = packageName.indexOf("@") !== -1 && packageName.lastIndexOf("/") !== -1;
         return nameIsScoped ?
-            packageName.slice(packageName.lastIndexOf("/")) :
+            packageName.slice(packageName.lastIndexOf("/") + 1) :
             packageName;
     }
 
